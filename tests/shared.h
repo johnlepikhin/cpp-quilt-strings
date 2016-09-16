08654a792c513aed5f872a3e8582791612ca740b
Evgenii Lepikhin <johnlepikhin@gmail.com>
2016-09-16 12:49:06 +0300

    Add missing compilation unit

---
+++ tests/shared.h
@@ -0,0 +1,11 @@
+
+#ifndef TESTS_SHARED_H_
+#define TESTS_SHARED_H_
+
+#include "../src/Quilt.h"
+#include "../src/Ternary.h"
+
+extern std::string *quilt_data1;
+extern Quilt *quilt1;
+
+#endif /* TESTS_SHARED_H_ */
